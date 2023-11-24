05635e7248350ac7ab3375be52fe6f88ab65f2b7
Israel Igarashi <89955427+igarashiisrael@users.noreply.github.com>
2023-11-24 19:51:17 -0300

    footer update

--- globalsolution/src/components/Footer/Footer.js
+++ globalsolution/src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
-import './Footer.css'
+import './Footer.css';
+
 function Footer() {
 
     const validarEmail = () => {
-        var emailInput = document.querySelector('.input-email');
+        var emailInput = document.querySelector('.footer-input-email');
         var email = emailInput.value;
         var emailRegex = /\S+@\S+\.\S+/;
 
@@ -14,7 +15,7 @@ function Footer() {
     }
 
     return (
-        <footer className="text-center text-lg-start fundo-verde text-muted pt-1">
+        <footer className="footer-fundo-verde text-center text-lg-start text-muted pt-1">
             <section>
                 <div className="container text-center text-md-start mt-5">
                     <div className="row mt-3">
@@ -22,7 +23,7 @@ function Footer() {
                             <h6 className="text-uppercase fw-bold mb-4">
                                 <i className="fas fa-gem me-3 text-secondary"></i>SaudeAPP
                             </h6>
-                            <p>
+                            <p className="footer-text-reset">
                                 O SaúdeApp é uma inovação no campo da saúde, proporcionando acesso simplificado e eficiente a serviços médicos essenciais. Suas funcionalidades abrangentes estão redefinindo a forma como as pessoas gerenciam suas informações médicas e interagem com o Sistema Único de Saúde (SUS).
                             </p>
                         </div>
@@ -32,25 +33,25 @@ function Footer() {
                                 Nossa Equipe
                             </h6>
                             <p>
-                                <a href="#!" className="text-reset">Israel Igarashi - RM 98359</a>
+                                <a href="#!" className="footer-text-reset">Israel Igarashi - RM 98359</a>
                             </p>
                             <p>
-                                <a href="#!" className="text-reset">Leonardo Bernardes - RM 99788</a>
+                                <a href="#!" className="footer-text-reset">Leonardo Bernardes - RM 99788</a>
                             </p>
                             <p>
-                                <a href="#!" className="text-reset">Matheus Gouvêa - RM 97774</a>
+                                <a href="#!" className="footer-text-reset">Matheus Gouvêa - RM 97774</a>
                             </p>
                             <p>
-                                <a href="#!" className="text-reset">Ryan Aquino - RM 550422</a>
+                                <a href="#!" className="footer-text-reset">Ryan Aquino - RM 550422</a>
                             </p>
                         </div>
 
                         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                             <h6 className="text-uppercase fw-bold mb-4">Newsletter</h6>
-                            <p>Cadastre seu e-mail ou telefone e fique por dentro das novidades sobre nossa plataforma!</p>
-                            <input type="text" placeholder="Digite seu e-mail" className="form-control mb-3 input-email" />
-                            <input type="text" placeholder="Digite seu telefone" className="form-control mb-3 input-telefone valida-telefone" />
-                            <button className="btn" onClick={validarEmail}>Inscrever-se</button>
+                            <p>Cadastre seu e-mail ou telefone...</p>
+                            <input type="text" placeholder="Digite seu e-mail" className="form-control mb-3 footer-input-email" />
+                            <input type="text" placeholder="Digite seu telefone" className="form-control mb-3 footer-input-telefone" />
+                            <button className="btn footer-btn" onClick={validarEmail}>Inscrever-se</button>
                         </div>
 
                         <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
@@ -58,26 +59,21 @@ function Footer() {
                                 Patrocinadores do FIAP - GLOBAL SOLUTION
                             </h6>
                             <p>
-                                <a href="https://www.fiap.com.br/" className="text-reset">FIAP</a>
+                                <a href="https://www.fiap.com.br/" className="footer-text-reset">FIAP</a>
                             </p>
                             <p>
-                                <a href="https://www.hapvidandi.com.br/" className="text-reset">Hapvida NotreDame Intermédica</a>
-                            </p>
-                            <p>
-
+                                <a href="https://www.hapvidandi.com.br/" className="footer-text-reset">Hapvida NotreDame Intermédica</a>
                             </p>
                         </div>
                     </div>
-
-
                 </div>
             </section>
 
             <div className="text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.025)" }}>
-                © 2023 Copyright:
-                <a className="text-reset fw-bold" href="#!">SaudeAPP</a>
+                © 2023 Copyright: SaudeAPP
             </div>
         </footer>
     );
 }
+
 export default Footer;
--- globalsolution/src/components/MortalidadeInfantil/MortalidadeInfantil.js
+++ globalsolution/src/components/MortalidadeInfantil/MortalidadeInfantil.js
@@ -10,20 +10,22 @@ function MortalidadeInfantil() {
 
             <section className="features bg-info py-5">
                 <div className="container">
-                    <h2 className="text-center mb-4">Informações sobre Mortalidade Infantil</h2>
+                    <h2 className="text-center mb-4">Redução da Mortalidade Materna e Infantil no Brasil</h2>
                     <div className="row text-center">
-                        {/* Exemplo de bloco de informação */}
                         <div className="col-md-4 mb-3">
-                            <h3>Causas Comuns</h3>
-                            <p>As principais causas incluem complicações durante o parto, infecções como pneumonia e diarréia, e nascimento prematuro.</p>
+                            {/* <h3>Causas Comuns</h3> */}
+                            <p>De 1990 a 2019 houve redução da TMI no Brasil e em todas as Regiões. Em 2019, no Brasil, estima-se que ocorreram 38.619 óbitos infantis, implicando em uma taxa de cobertura de óbito infantil de 91,4% e uma TMI de 13,3 óbitos por mil NV, retornando ao mesmo patamar de 2015. As maiores reduções foram observadas nos estados da Região Nordeste. Em 2019, a menor e a maior TMI foram estimadas para o Distrito Federal e o Amapá, com 8,5 e 22,9 óbitos para cada mil NV, respectivamente.</p>
                         </div>
                         <div className="col-md-4 mb-3">
-                            <h3>Impacto Global</h3>
-                            <p>A mortalidade infantil é um indicador crítico de saúde e bem-estar em uma sociedade. A maioria das mortes ocorre em países em desenvolvimento.</p>
+                            {/* <h3>Impacto Global</h3> */}
+                            <p>Em todas as Regiões, exceto a Região Sul, houve um discreto aumento da TMI em 2016 ocasionado, provavelmente, pela epidemia de vírus Zika, que levou à queda da natalidade e à morte de bebês com malformações graves, e a crise econômica, que ocasionou um aumento no número de óbitos por causas evitáveis10. Dos óbitos notificados ao SIM em 2019, 80,3% foram investigados, variando de 55% na Bahia até 99% no Distrito Federal.</p>
                         </div>
-                        <div className="col-md-4 mb-3">
-                            <h3>Prevenção</h3>
-                            <p>Muitas mortes podem ser prevenidas com acesso a serviços de saúde de qualidade, nutrição adequada e práticas de higiene.</p>
+                        {<div className="col-md-4 mb-3">
+                            {/* <h3>Prevenção</h3> */}
+                            <p>Ao longo de 2020, houve uma notável redução da natalidade no Brasil, de modo que, ao todo, 2.730.145 nascimentos foram registrados – a tendência de queda foi observada em todas as regiões do País. Dos fetos nascidos vivos, 71% vieram de mulheres que realizaram sete ou mais consultas de pré-natal durante a gestação, e 78,5% nasceram de mulheres que iniciaram o pré-natal no 1º trimestre gestacional, indicando um pequeno aumento de 0,1% em relação aos dados obtidos em 2019. </p>
+                        </div>}
+                        <div className="col-md-12 mb-3">
+                            <p>Por outro lado, 7,8% dos nascidos vivos em 2020 descenderam de mulheres que realizaram no máximo três consultas de pré-natal durante toda a gestação. Este cenário aponta uma grande diferença regional e reflete divergências sociais e econômicas, visto que 18% dos casos ocorreram no Norte, contra 4% no Sul. </p>
                         </div>
                     </div>
                 </div>
